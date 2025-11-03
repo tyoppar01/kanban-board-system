6c0dc3b30f86e8c666917e7eccbe7892c4741f57
jk <jayaw5508@gmail.com>
2025-11-04 00:33:25 +0800

    Add standard middleware to start server (waiting to debug)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,37 @@
 import express = require("express");
 import http = require("http");
+import cors = require("cors");
+
+const boardRouter = require("./routes/boardRoute.ts");
+const taskRouter = require ("./routes/taskRoute.ts");
 
 const app = express();
 app.use(express.json());
 
-const server = http.createServer(app);
+// ==================== Middleware ======================== //
+app.use(cors());
+app.use(express.json());
+
+// =================== Health Check ======================= //
+app.get("/", (req, res) => {
+  console.log("✅ GET / triggered");
+  res.status(200).json({ success: true, message: "Server is running" });
+});
+
+// ===================== Error Check ========================= //
+
+
+
+// =================== Server Setup ======================= //
+
+const PORT = 8080;
+const server = app.listen(PORT, () => 
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
+
 
 // timeout configuration
 server.requestTimeout = 120000;     
 server.headersTimeout = 60000;      
 server.keepAliveTimeout = 10000;    
-server.timeout = 0;                
-
-app.get("/", (req, res) => {
-  res.send("🚀 Server running successfully!");
-});
-
-app.listen(3000, () => console.log("✅ Server running on http://localhost:3000"));
+server.timeout = 0;    
