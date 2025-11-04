85360b31a3d09671b9e5ffa6ace54565a6e32ebb
kaile <kai_le.aw@bp.com>
2025-11-04 14:04:33 +0800

    Add board router to redirect server to board controller

--- server/src/routes/boardRoute.ts
+++ server/src/routes/boardRoute.ts
@@ -1,12 +1,17 @@
-import express = require("express");
-import getBoard = require("../controllers/boardController");
+import express, { Request, Response } from "express";
+import { getFullBoard } from "../services/boardService";
 
-const router = express.Router();
+const boardRouter = express.Router();
 
 // GET /api/board/
-router.get("/", getBoard);
+boardRouter.get("/", async (_, res: Response) => {
+      console.log("GET /api/board called");
+      try {
+            console.log("GET successss");
+            return res.json(await getFullBoard());
+      } catch (err: any) {
+            return res.status(404).json({ success: false, message: err.mes });
+      }
+});
 
-
-
-
-export default router;
+export default boardRouter;
