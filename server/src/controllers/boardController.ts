fe23e6f9dbe50ee2e29f9078772e9b39b889719d
kaile <kai_le.aw@bp.com>
2025-11-04 14:02:45 +0800

    Setup board controller logic

--- server/src/controllers/boardController.ts
+++ server/src/controllers/boardController.ts
@@ -1,13 +1,14 @@
-import type { Request, Response } from "express";
-import boardService = require("../services/boardService")
+import express, { Request, Response } from "express";
+import boardService = require("../services/boardService");
 
 export const getBoard = async function (req: Request, res: Response){
 
     try {
-        const boards = await boardService.getFullBoard();
-        res.status(200).json({ success: false, statusCode: 200, message: board });
+        const board = await boardService.getFullBoard();
+        console.log(JSON.stringify(board));
+        return res.status(200).json({ success: false, statusCode: 200, message: board });
     } catch (err: any) {
-        res.status(500).json({ success: false, statusCode: 500, message: err.message });
+        return res.status(500).json({ success: false, statusCode: 500, message: err.message });
     }
 
 }
