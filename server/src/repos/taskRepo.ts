489331a1c3af60d89c87e1a7a2c2170dd97f5afb
jk <jayaw5508@gmail.com>
2025-11-04 23:00:46 +0800

    Update task repo and service to seperate responsibility of validation and modification

--- server/src/repos/taskRepo.ts
+++ server/src/repos/taskRepo.ts
@@ -11,25 +11,13 @@ export const taskRepo = {
    * @param task 
    * @returns 
    */
-  add: async (task: Task): Promise<Record<number, Task>> => {
-
-    // ================================= GET OPERATION ================================= //
-
-    // get board object
-    const board = await boardRepo.get();
-
-    // get todo column list where consists of todo ids
-    const todoColumnList = board.columns["todo"] ?? [];
-
-    // ============================== INSERT OPERATION ============================== //
+  add: (board: Board, task: Task): Record<number, Task> => {
 
     // append id into todo column list
-    todoColumnList.push(task.id);
+    board.columns["todo"]!.push(task.id);
 
     // get taskList where consists of tasks
     board.taskList[task.id] = task;
-
-    // ============================= COMPLETED OPERATION ============================= //
 
     return board.taskList;
   },
@@ -40,35 +28,13 @@ export const taskRepo = {
    * @param column 
    * @returns 
    */
-  remove: async (taskId: number, column: string): Promise<Task> => {
+  remove: (taskId: number, column: string, board: Board): Task => {
     
-    // get board object
-    const board = await boardRepo.get();
-
-    // iterate the column list to remove task id
-    // NOTE: task is preserved in taskList for further restore implementation
-    const colList = board.columns[column]
-    
-    // throw Error if column is not exist
-    if (!colList) {
-      throw new Error(`Column ${column} not found!`);
-    }
-
     // remove taskId from the column list
-    board.columns[column] = colList.filter((id) => id !== taskId);
-
-
-    // persist changes
+    board.columns[column] = board.columns[column]!.filter((id) => id !== taskId);
 
     // return the object
-    const removedTask = board.taskList[taskId];
-
-    // ensure that it is preserved, unless implement otherwise
-    if (!removedTask) {
-      throw new Error(`Task ${taskId} is not found in task list`);
-    }
-
-    return removedTask;
+    return board.taskList[taskId]!;
   },
 
   /**
@@ -79,27 +45,11 @@ export const taskRepo = {
    * @param destCol 
    * @returns 
    */
-  updateColumn: async (taskId: number, index: number, currCol: string, destCol: string): Promise<Task> => {
+  updateColumn: (taskId: number, index: number, currCol: string, destCol: string, board: Board): Task => {
     
-    // ================================= GET OPERATION ================================= //
-
-    const board = await boardRepo.get();
-
-    const currentList = board.columns[currCol];
-    const destinationList = board.columns[destCol];
-    
-    // =============================== VALIDATION OPERATION =============================== //
-
-    // if current list not exist or task id not found in list
-    if (!currentList || !currentList.includes(taskId)) throw new Error(`Column ${currCol} not found!`);
-    
-    // if destination list not exist
-    if (!destinationList) throw new Error(`Column ${destCol} not found!`);
-  
-    // if index out of bound
-    if (index < 0 || index > destinationList.length) throw new Error(`Invalid index ${index}: must be between 0 and ${destinationList.length}`);
-    
-    // ============================== UPDATE OPERATION ============================== //
+    // retrieve current and destination arrays
+    const currentList = board.columns[currCol]!;
+    const destinationList = board.columns[destCol]!;
 
     // remove taskId from the column list
     board.columns[currCol] = currentList.filter((id) => id !== taskId);
@@ -113,16 +63,7 @@ export const taskRepo = {
 
     board.columns[destCol] = newDestinationList;
 
-    // ============================= COMPLETED OPERATION ============================= //
-
-    const updatedTask = board.taskList[taskId];
-
-    // ensure that it is preserved, unless implement otherwise
-    if (!updatedTask) {
-      throw new Error(`Task ${taskId} is not found in task list`);
-    }
-
-    return updatedTask;
+    return board.taskList[taskId]!;
   },
 
 };
