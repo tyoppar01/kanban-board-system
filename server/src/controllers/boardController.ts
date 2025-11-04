7f77adba07d1edfa9679ff03cdd11576d3511991
jk <jayaw5508@gmail.com>
2025-11-04 18:26:22 +0800

    Update board controller based on new response object generated

--- server/src/controllers/boardController.ts
+++ server/src/controllers/boardController.ts
@@ -1,29 +1,28 @@
 import { Request, Response } from "express";
-import {getFullBoard} from "../services/boardService";
 import { ApiResponse } from "../../types/response";
-import { sendSuccessResponse, sendFailedResponse } from "../../utils/apiResponse";
+import { sendFailedResponse, sendSuccessResponse } from "../../utils/apiResponse";
+import { ApiStatus } from "../../utils/apiStatus";
+import { ErrorCode } from "../../utils/errorCode";
+import { getFullBoard } from "../services/boardService";
 
-export const getBoard = async function (_req: Request, res: Response){
+export const getBoard = async function (_req: Request, _res: Response){
 
     try {
+        // GET board
         const board = await getFullBoard();
 
-        // create a generic response
-        const response: ApiResponse<typeof board> = {
-                success: true,
-                message: "Board fetched successfully",
-                data: board
-        };
-        sendSuccessResponse(res, response);
+        // Generate GOOD Response
+        const response: ApiResponse<any> = {
+            success: true,
+            message: "board details has been received successfully",
+            data: board
+        }
+
+        // Return Response
+        sendSuccessResponse(_res, response);
 
     } catch (err: any) {
-
-        // create a generic error response
-        const errorResponse: ApiResponse<any> = {
-                success: false,
-                message: "Internal Server Error"
-        };
-        sendFailedResponse(res, errorResponse);
+        sendFailedResponse(_res, ApiStatus.SYSTEM_ERROR, ErrorCode.SYSTEM_ERROR);
     }
 
 }
