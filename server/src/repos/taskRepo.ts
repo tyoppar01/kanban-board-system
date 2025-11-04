a10e0a192e2e4b689619b49e009c1a4038dc2a44
jk <jayaw5508@gmail.com>
2025-11-04 22:11:05 +0800

    Add updateColumn operation to update task via drag and drop feature

--- server/src/repos/taskRepo.ts
+++ server/src/repos/taskRepo.ts
@@ -6,7 +6,14 @@ import { boardRepo } from "./boardRepo";
 
 export const taskRepo = {
 
+  /**
+   * Add New Task
+   * @param task 
+   * @returns 
+   */
   add: async (task: Task): Promise<Record<number, Task>> => {
+
+    // ================================= GET OPERATION ================================= //
 
     // get board object
     const board = await boardRepo.get();
@@ -14,13 +21,108 @@ export const taskRepo = {
     // get todo column list where consists of todo ids
     const todoColumnList = board.columns["todo"] ?? [];
 
+    // ============================== INSERT OPERATION ============================== //
+
     // append id into todo column list
     todoColumnList.push(task.id);
 
     // get taskList where consists of tasks
     board.taskList[task.id] = task;
 
+    // ============================= COMPLETED OPERATION ============================= //
+
     return board.taskList;
-  }
+  },
+
+  /**
+   * Remove an existing task
+   * @param taskId 
+   * @param column 
+   * @returns 
+   */
+  remove: async (taskId: number, column: string): Promise<Task> => {
+    
+    // get board object
+    const board = await boardRepo.get();
+
+    // iterate the column list to remove task id
+    // NOTE: task is preserved in taskList for further restore implementation
+    const colList = board.columns[column]
+    
+    // throw Error if column is not exist
+    if (!colList) {
+      throw new Error(`Column ${column} not found!`);
+    }
+
+    // remove taskId from the column list
+    board.columns[column] = colList.filter((id) => id !== taskId);
+
+
+    // persist changes
+
+    // return the object
+    const removedTask = board.taskList[taskId];
+
+    // ensure that it is preserved, unless implement otherwise
+    if (!removedTask) {
+      throw new Error(`Task ${taskId} is not found in task list`);
+    }
+
+    return removedTask;
+  },
+
+  /**
+   * Update Task (Drag and Drop feature)
+   * @param taskId 
+   * @param index 
+   * @param currCol 
+   * @param destCol 
+   * @returns 
+   */
+  updateColumn: async (taskId: number, index: number, currCol: string, destCol: string): Promise<Task> => {
+    
+    // ================================= GET OPERATION ================================= //
+
+    const board = await boardRepo.get();
+
+    const currentList = board.columns[currCol];
+    const destinationList = board.columns[destCol];
+    
+    // =============================== VALIDATION OPERATION =============================== //
+
+    // if current list not exist or task id not found in list
+    if (!currentList || !currentList.includes(taskId)) throw new Error(`Column ${currCol} not found!`);
+    
+    // if destination list not exist
+    if (!destinationList) throw new Error(`Column ${destCol} not found!`);
+  
+    // if index out of bound
+    if (index < 0 || index > destinationList.length) throw new Error(`Invalid index ${index}: must be between 0 and ${destinationList.length}`);
+    
+    // ============================== UPDATE OPERATION ============================== //
+
+    // remove taskId from the column list
+    board.columns[currCol] = currentList.filter((id) => id !== taskId);
+
+    // insert task id at selected index at destination list
+    const newDestinationList: number[] = [
+      ...destinationList.slice(0, index),
+      taskId,
+      ...destinationList.slice(index)
+    ];
+
+    board.columns[destCol] = newDestinationList;
+
+    // ============================= COMPLETED OPERATION ============================= //
+
+    const updatedTask = board.taskList[taskId];
+
+    // ensure that it is preserved, unless implement otherwise
+    if (!updatedTask) {
+      throw new Error(`Task ${taskId} is not found in task list`);
+    }
+
+    return updatedTask;
+  },
 
 };
--- server/src/services/taskService.ts
+++ server/src/services/taskService.ts
@@ -10,3 +10,15 @@ export const addTask = async (task: Task): Promise<Record<number, Task>> => {
     const board = await taskRepo.add(task);
     return board;
 }
+
+export const removeTask = async (id: number, column: string): Promise<Task> => {
+
+    const deletedTask:Task = await taskRepo.remove(id, column);
+    return deletedTask;
+}
+
+export const relocateTask = async (id: number, index: number, currCol: string, destCol: string): Promise<Task> => {
+
+    const relocatedTask:Task = await taskRepo.updateColumn(id, index, currCol, destCol);
+    return relocatedTask;
+}
