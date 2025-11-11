81be1f815cec38590e79f595c104256e4cacb1cc
jk <jayaw5508@gmail.com>
2025-11-12 00:26:54 +0800

    Update mongodb index file

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,18 @@
-import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import path from 'path';
+
 dotenv.config();
 
 export const conenctMongoose = async () => {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("Starting to connect to MongoDB")
+        await mongoose.connect(process.env.MONGO_URI!, 
+            { 
+                family: 4,
+                serverSelectionTimeoutMS: 10000 
+            });
         console.log('✅ Connected to MongoDB');
     } catch (err) {
         console.error("❌ MongoDB connection failed:", err);
