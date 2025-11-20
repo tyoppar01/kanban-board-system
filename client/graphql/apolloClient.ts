62bc4e634cf65d2526c71d8ae44342a906638766
Najiha Zaharuddin <najihazaharuddin@Najihas-MacBook-Air.local>
2025-11-21 00:32:59 +0800

    remove hardcoding

--- client/graphql/apolloClient.ts
+++ client/graphql/apolloClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }), 
+  link: new HttpLink({ 
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:8080/graphql' 
+  }), 
   cache: new InMemoryCache(),
 });
 
